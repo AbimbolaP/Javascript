f6b8d29d5f488d660c23c62c31b1d5c0767fc562
AbimbolaP <88383628+AbimbolaP@users.noreply.github.com>
2022-06-24 14:17:33 +0100

    Javascript

---
+++ introduction.js
@@ -0,0 +1,4 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+console.log('Name: Otegbayo Abimbola');
+console.log('Height: 1.62');
+console.log('Country: Nigeria');
